7f42e7cb3a71e71fd6bed59148c08ba5f9b7721e
Crabar <crabar0@gmail.com>
2016-08-16 17:14:32 +0300

    added share-panel

---
+++ addon/components/share-panel.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import layout from '../templates/components/share-panel';
+
+export default Ember.Component.extend({
+  layout,
+  buttonToComponent: {'fb': 'fb-share-button',
+                      'facebook': 'fb-share-button',
+                      'vk': 'vk-share-button',
+                      'vkontakte': 'vk-share-button',
+                      'twitter': 'twitter-share-button'},
+  buttons: '',
+  labels: '',
+  components: Ember.computed('buttons', function() {
+    const buttons = this.get('buttons').split(',').map((item) => item.trim());
+    const labels = this.get('labels').split(',').map((item) => item.trim());
+    return buttons.map((item, index) =>
+      ({name: this.get('buttonToComponent')[item], label: labels[index]}));
+  })
+});
