1f1a749ab96952ae21d5317b3886acc7c97c706e
Yaroslav Poliakov (Crabar) <ypoliakov@playtika.com.ua>
2016-08-31 11:43:21 +0300

    improved styles; fixed error with long labels;

--- addon/components/share-panel.js
+++ addon/components/share-panel.js
@@ -3,6 +3,7 @@ import layout from '../templates/components/share-panel';
 
 export default Ember.Component.extend({
   layout,
+  classNames: ['share-panel'],
   buttonToComponent: {'fb': 'fb-share-button',
                       'facebook': 'fb-share-button',
                       'vk': 'vk-share-button',
@@ -11,7 +12,7 @@ export default Ember.Component.extend({
   buttons: '',
   labels: '',
   adaptive: true,
-  
+
   components: Ember.computed('buttons', function() {
     const buttons = this.get('buttons').split(',').map((item) => item.trim());
     const labels = this.get('labels').split(',').map((item) => item.trim());
